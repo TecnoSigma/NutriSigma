0f8ef60986634a93ef1f0f4300665cd8c7044ec7
Yomestre <Yomestre@hotmail.com>
2015-03-03 14:33:34 -0300

    Criado controller e suas rotas

--- spec/models/patient_spec.rb
+++ spec/models/patient_spec.rb
@@ -2,12 +2,14 @@ require 'rails_helper'
 
 RSpec.describe Patient, :type => :model do
   let(:valid_patient) {{patient_id: 00000000, name:"paciente", email:"paciente@patient.com", gender:"Masculino", age:30, height: 3.42, weight: 80}}
-  describe "validates ´presence" do
+  describe "Validates presence" do
     it "Should save a valid patient" do
       @patient = Patient.new(valid_patient)
       expect(@patient.valid?).to eq(true)
     end
     it "Should NOT save an invalid patient" do
+      @patient = Patient.new
+      expect(@patient.valid?).to eq(false)
     end
   end
 end
