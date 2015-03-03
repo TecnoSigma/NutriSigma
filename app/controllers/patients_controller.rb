0524e1e40f58b00c5caa2c875b087e25ef23bc65
Yomestre <Yomestre@hotmail.com>
2015-03-03 15:01:13 -0300

    Criado controller e suas rotas

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,22 +1,50 @@
 class PatientsController < ApplicationController
   def index
+    @patients = Patient.all
   end
 
   def show
+    @patient = Patient.find(params[:id])
   end
 
   def new
+    @patient = Patient.new
   end
 
   def create
+    @patient = Patient.new(patient_params)
+    save_patient
   end
 
   def edit
+    @patient = Patient.find(params[:id])
   end
 
   def update
+    @patient = Patient.find(params[:id])
+    update_patient
   end
 
   def destroy
+    @patient = Patient.find(params[:id]).destroy
+    redirect_to patients_path
+  end
+private
+  def patient_params
+    params.require(:patient).permit(:patient_id, :name, :email, :gender, :age, :height, :weight )
+  end
+  def save_patient
+    if @patient.save
+      redirect_to @patient
+    else
+      render "new"
+    end
+  end
+  def update_patient
+    if @patient.update(patient_params)
+      redirect_to @patient
+    else
+      render "edit"
+    end
   end
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,3 +1,3 @@
 class Patient < ActiveRecord::Base
-	validates :patient_id, :name, :email, :gender, :age, :height, :weight,presence: true
+	validates :patient_id, :name, :email, :gender, :age, :height, :weight, presence: true
 end
