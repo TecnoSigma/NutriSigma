ca0effbe93f37e70957cbba1e8a81efbad3c7b99
Yomestre <Yomestre@hotmail.com>
2015-03-03 20:58:36 -0300

    Rspec post actions patients_controller

--- spec/controllers/patients_controller_spec.rb
+++ spec/controllers/patients_controller_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe PatientsController, :type => :controller do
+  let(:patient_params) {{patient_id: 00000000, name:"paciente2", email:"pacient3@patient.com", gender:"Masculino", age:30, height: 3.42, weight: 80 }}
   before do
     10.times do |n|
       @a_patient = Patient.create(patient_id: 00000000, name:"paciente", email:"paciente@patient.com", gender:"Masculino", age:30, height: 3.42, weight: 80 )
@@ -21,8 +22,18 @@ RSpec.describe PatientsController, :type => :controller do
       expect(response).to render_template(:new)
     end
     it "#edit" do
-      get :edit
+      get :edit, id:1
       expect(response).to render_template(:edit)
+    end
+  end
+  describe "POST action" do
+    it "#create" do
+      post :create, patient: patient_params
+      expect(Patient.last.name).to eq("paciente2")
+    end
+    it "#update" do 
+      post :update, id: 10, patient:{name:"pacient3"}
+      expect(Patient.find(10).name).to eq("pacient3")
     end
   end
 end
