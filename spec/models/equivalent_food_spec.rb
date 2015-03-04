3df6cb30fd592b1d486bc741bf89498d519c29d1
Alexandre <tecnoalpha@ig.com.br>
2015-03-04 01:16:48 -0300

    Refatoração do teste 'validate creation of the Equivalent Food' funcional!

--- spec/models/equivalent_food_spec.rb
+++ spec/models/equivalent_food_spec.rb
@@ -1,14 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe EquivalentFood, :type => :model do
+  
+  let(:food) {{food: "Cenoura cozida",
+               group: 2,
+               quantity_measure: 4,
+               measure: "colheres de sopa",
+               weight: 100}}
+
   describe "validate Equivalent Group" do
   
     it "validate creation of the Equivalent Food" do
-      f = EquivalentFood.create(food: "Cenoura cozida",
-                                group: 2,
-                                quantity_measure: 4,
-                                measure: "colheres de sopa",
-                                weight: 100)
+      f = EquivalentFood.create(food)
       expect(f.persisted?).to be true
     end
 
