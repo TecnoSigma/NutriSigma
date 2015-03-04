46872af12e4cff484c63e33540e520812c180387
Alexandre <tecnoalpha@ig.com.br>
2015-03-04 00:48:31 -0300

    Model Food Groups criado com sucesso!

---
+++ db/migrate/20150304031442_create_equivalent_foods.rb
@@ -0,0 +1,14 @@
+class CreateEquivalentFoods < ActiveRecord::Migration
+  def change
+    create_table :equivalent_foods do |t|
+      t.string :food
+      t.string :group
+      t.string :integer
+      t.integer :quantity_measure
+      t.string :measure
+      t.integer :weight
+
+      t.timestamps null: false
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,15 +11,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150303223753) do
+ActiveRecord::Schema.define(version: 20150304034625) do
 
   create_table "equivalent_foods", force: :cascade do |t|
     t.string   "food"
+    t.string   "group"
+    t.string   "integer"
     t.integer  "quantity_measure"
     t.string   "measure"
     t.integer  "weight"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+  end
+
+  create_table "food_groups", force: :cascade do |t|
+    t.string   "group"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
