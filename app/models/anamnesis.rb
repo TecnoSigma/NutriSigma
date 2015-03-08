2e2219d733bef5a18a97504c18a3d6f108c973b9
Yomestre <Yomestre@hotmail.com>
2015-03-08 12:23:48 -0300

    Mudança nos models Patients e Anamnesis

--- app/models/anamnesis.rb
+++ app/models/anamnesis.rb
@@ -1,4 +1,4 @@
 class Anamnesis < ActiveRecord::Base
-  validates :medical_register_id,:morning_meal_time,:noon_meal_time,:evening_meal_time,:patient_id,presence:true
-  belongs_to :patient
+  validates :medical_register_id, :morning_meal_time, :noon_meal_time, :evening_meal_time, :patient_id, presence:true
+  belongs_to :patient, dependent: :destroy
 end
--- app/models/patient.rb
+++ app/models/patient.rb
@@ -1,4 +1,4 @@
 class Patient < ActiveRecord::Base
   validates :patient_id, :name, :email, :gender, :age, :height, :weight, presence: true
-  has_one :anamnesis
+  has_one :anamnesis 
 end
