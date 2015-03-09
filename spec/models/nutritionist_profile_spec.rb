e3406cd3a4ae7f431693cf249630fac2963e1041
Alexandre <tecnoalpha@ig.com.br>
2015-03-09 00:31:59 -0300

    rspec 'validate creation nutritionist profile' funcional!

--- spec/models/nutritionist_profile_spec.rb
+++ spec/models/nutritionist_profile_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe NutritionistProfile, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:profile) {{name: "Maria da Silva",
+                  email: "maria.silva@nutri.com.br",
+                  issuing_institution: "CRN-3",
+                  crn_number: "0244",
+                  expiration_crn: "2018-11-05",
+                  license_type: "Definitiva"}}
+
+  describe "Validate Nutritionist Profile" do
+
+    it "validate creation nutritionist profile" do
+      n = NutritionistProfile.create(profile)
+      expect(n.persisted?).to be true
+    end
+
+  end
+
 end
