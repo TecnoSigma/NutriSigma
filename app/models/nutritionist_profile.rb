ba6f095f714d71e0b401a1f193598d9e2811ad74
Alexandre <tecnoalpha@ig.com.br>
2015-03-09 00:51:41 -0300

    Atualização do model

--- app/models/nutritionist_profile.rb
+++ app/models/nutritionist_profile.rb
@@ -1,3 +1,3 @@
 class NutritionistProfile < ActiveRecord::Base
-  validates_presence_of :name, :email, :issuing_institution, :crn_number, :expiration_crn, :license_type
+  validates_presence_of :name, :email, :issuing_institution, :crn_number, :crn_expiration, :license_type
 end
