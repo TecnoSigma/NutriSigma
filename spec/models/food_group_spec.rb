c1817ca8051046f7a24424d8837c5aaae03e423b
Alexandre <tecnoalpha@ig.com.br>
2015-03-08 22:43:45 -0300

    rspec 'validate creation of the food group' funcional'!

--- spec/models/food_group_spec.rb
+++ spec/models/food_group_spec.rb
@@ -1,16 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe FoodGroup, :type => :model do
+  let(:food_group)  {{group: "Frutas",
+                      carbohydrate: 16,
+                      proteins: 0.5,
+                      lipids: 0.6,
+                      kilocalories: 75.0}}
+
   describe "validate Food Group" do
     
     it "validate creation of the food group" do
-      g = FoodGroup.create(group: "Sementes")
+      g = FoodGroup.create(food_group)
       expect(g.persisted?).to be true
-    end
-
-    it "validate creation of the food group (empty)" do
-      g = FoodGroup.create
-      expect(g.persisted?).to be false
     end
     
   end
