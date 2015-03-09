dfeb24b00f6c4c71ab5ba3e1b85cc405df3b8827
Alexandre <tecnoalpha@ig.com.br>
2015-03-09 20:07:09 -0300

    rspec 'validate creation of the Nutritionist Profile (field issuing_institution is valid using the option 'CRN-9')' funcional!

--- spec/models/nutritionist_profile_spec.rb
+++ spec/models/nutritionist_profile_spec.rb
@@ -139,6 +139,13 @@ RSpec.describe NutritionistProfile, :type => :model do
         n.issuing_institution = "CRN-8"
         expect(n.valid?).to be true
       end
+
+      it "validate creation of the Nutritionist Profile (field issuing_institution is valid using the option 'CRN-9')" do
+        n = NutritionistProfile.new(profile)
+        n.issuing_institution = "CRN-9"
+        expect(n.valid?).to be true
+      end
+
     end
 
   end
