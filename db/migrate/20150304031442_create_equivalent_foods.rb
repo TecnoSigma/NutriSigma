2485553fefa1cf6f5b2411d9e39d91c29aa1b66f
Yomestre <Yomestre@hotmail.com>
2015-03-09 19:30:56 -0300

    Retirado campo group de EquivalentFood

--- db/migrate/20150304031442_create_equivalent_foods.rb
+++ db/migrate/20150304031442_create_equivalent_foods.rb
@@ -2,7 +2,6 @@ class CreateEquivalentFoods < ActiveRecord::Migration
   def change
     create_table :equivalent_foods do |t|
       t.string :food
-      t.integer :group
       t.float :quantity_measure
       t.string :measure
       t.float :weight
