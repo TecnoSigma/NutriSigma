8cc7c97e7acfbfa710e5f5845d984bd4f2b74d28
Alexandre <tecnoalpha@ig.com.br>
2015-03-08 23:25:35 -0300

    Refatoração do model food_group.rb (validação de integers e floats)

--- app/models/food_group.rb
+++ app/models/food_group.rb
@@ -1,3 +1,7 @@
 class FoodGroup < ActiveRecord::Base
   validates_presence_of :group, :carbohydrate, :proteins, :lipids, :kilocalories
+  validates_numericality_of :carbohydrate, only_float: true
+  validates_numericality_of :proteins, only_float: true
+  validates_numericality_of :lipids, only_float: true
+  validates_numericality_of :kilocalories, only_float: true
 end
