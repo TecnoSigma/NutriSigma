d0e17d69abf26a0b68e996658f7049041ad72ed7
Alexandre <tecnoalpha@ig.com.br>
2015-03-09 00:50:17 -0300

    Alteração do nome do campo 'expiration_crn' para 'crn_expiration'

--- db/migrate/20150309024537_create_nutritionist_profiles.rb
+++ db/migrate/20150309024537_create_nutritionist_profiles.rb
@@ -5,7 +5,7 @@ class CreateNutritionistProfiles < ActiveRecord::Migration
       t.string :email
       t.string :issuing_institution
       t.string :crn_number
-      t.date :expiration_crn
+      t.date :crn_expiration
       t.string :license_type
 
       t.timestamps null: false
