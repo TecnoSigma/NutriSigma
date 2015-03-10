36652d85a2e4e788af5fbd9e4a2cc558a4e9e6ef
Alexandre <tecnoalpha@ig.com.br>
2015-03-10 00:38:44 -0300

    Criação do controller 'edit'

--- app/controllers/nutritionist_profiles_controller.rb
+++ app/controllers/nutritionist_profiles_controller.rb
@@ -1,7 +1,15 @@
 class NutritionistProfilesController < ApplicationController
   
   def index
-    
+
+  end
+
+  def edit
+    @nutriProfile = NutritionistProfile.find(params[:id])
+  end
+
+  def nutriprofile_params
+    params.require(:nutriprofile).permit(:name, :email, :issuing_institution, :crn_number, :crn_expiration, :license_type)
   end
 
 end
