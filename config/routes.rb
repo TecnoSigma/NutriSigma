f09f0f726ebe66b48824978de119d6c443d2d003
Alexandre <tecnoalpha@ig.com.br>
2015-03-09 21:30:34 -0300

    Criação do Nutritionist Profile (resources)

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   devise_for :nutritionists
   resources :patients
+  resources :nutritionist_profiles
+
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
