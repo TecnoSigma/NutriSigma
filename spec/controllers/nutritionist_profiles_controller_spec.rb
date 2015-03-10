c1eadfa6cf541a7a0a353c2bcd6ec46acd2f9042
Alexandre <tecnoalpha@ig.com.br>
2015-03-09 21:36:03 -0300

    rspec 'validate #index' funcional!

--- spec/controllers/nutritionist_profiles_controller_spec.rb
+++ spec/controllers/nutritionist_profiles_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe NutritionistProfilesController, :type => :controller do
+  let(:profile) {{name: "Maria da Silva",
+                  email: "maria.silva@nutri.com.br",
+                  issuing_institution: "CRN-3",
+                  crn_number: "0244",
+                  crn_expiration: "2018-11-05",
+                  license_type: "Definitiva"}}
+
+  describe "GET action" do
+
+    it "validate #index" do
+      get :index
+      expect(response).to have_http_status(:success)
+    end
+
+  end
 
 end
