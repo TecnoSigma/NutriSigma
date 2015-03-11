9f5d95d698f0e03429e812b778fcc467adb540a5
Alexandre <tecnoalpha@ig.com.br>
2015-03-10 21:30:58 -0300

    Criação do controller 'new'

--- app/controllers/nutritionist_profiles_controller.rb
+++ app/controllers/nutritionist_profiles_controller.rb
@@ -11,9 +11,18 @@ class NutritionistProfilesController < ApplicationController
   def show
     @nutriProfile = NutritionistProfile.find(params[:id])
   end
+
+  def create
+    @nutriProfile = NutritionistProfile.new(nutriprofile_params)
+    save_nutriprofile
+  end
   
   def nutriprofile_params
     params.require(:nutriprofile).permit(:name, :email, :issuing_institution, :crn_number, :crn_expiration, :license_type)
+  end
+
+  def new
+    @nutriprofile = NutritionistProfile.new
   end
 
 end
