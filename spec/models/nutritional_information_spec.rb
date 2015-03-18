08ede4888593aa1657c0b8631580891913b208a8
Alexandre <tecnoalpha@ig.com.br>
2015-03-17 21:46:54 -0300

    Criação do spec 'validate the % PTN (Percent of the Protein)'

--- spec/models/nutritional_information_spec.rb
+++ spec/models/nutritional_information_spec.rb
@@ -8,13 +8,25 @@ RSpec.describe NutritionalInformation, :type => :model do
                           height: 1.8, 
                           weight: 80}}
 
+    before do
+      phisical_activity = 1.12
+      @kcal = 1026
+      @tee = NutritionalInformation.tee(subject.age, phisical_activity, subject.weight, subject.height, @kcal)
+    end
+
     describe NutritionalInformation do
       subject{ Patient.new(valid_patient) }
 
       it "validate the TEE (Total Energy Expenditure)" do
-        phisical_activity = 1.12
-        kcal = 1026
-        expect(NutritionalInformation.tee(subject.age, phisical_activity, subject.weight, subject.height, kcal)).to eq (1733.32)
+        expect(@tee).to eq (1733.32)
+      end
+
+      it "validate the PTN (Protein)" do
+        expect(NutritionalInformation.ptn(subject.weight)).to eq (64.0)
+      end
+
+      it "validate % PTN (Percent of the Protein)" do
+        expect(NutritionalInformation.percent_ptn(@kcal,@tee)).to eq (59.19)
       end
 
     end
