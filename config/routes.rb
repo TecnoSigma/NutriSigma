0a31b6787cf72b3a1a86dc8ac3a47d447ee8d956
Alexandre <tecnoalpha@ig.com.br>
2015-03-18 01:27:50 -0300

    Refatoração do routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :nutritionists
   resources :patients
   resources :nutritionist_profiles
+  resources :nutritional_informations, only: [:show]
 
 
   # The priority is based upon order of creation: first created -> highest priority.
