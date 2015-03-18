af901fbf0df5271a9f7461f39fa8800d8dc409de
Alexandre <tecnoalpha@ig.com.br>
2015-03-18 00:33:20 -0300

    Criação do model 'RES. PTN (Residual of the Protein)'

--- app/models/nutritional_information.rb
+++ app/models/nutritional_information.rb
@@ -1,7 +1,38 @@
 class NutritionalInformation
+  #attr_reader :patient
+
+  #def initialize(patient)
+  #  @patient = patient
+  #end
+
+#def tee
+#  (1086 - (10.1 * age) + phisical_activity * (13.7 * weight) + (416 * height) - kcal).round(2)
+#end
+
+#def ptn
+  #0.8 * patient.weight 
+#end
+
 
   #Total Energy Expenditure
   def self.tee(age, phisical_activity, weight, height, kcal)
-    tee = (1086 - (10.1 * age) + phisical_activity * (13.7 * weight) + (416 * height) - kcal).round(2)
+    (1086 - (10.1 * age) + phisical_activity * (13.7 * weight) + (416 * height) - kcal).round(2)
   end
+
+  #PTN (Protein)
+  def self.ptn(weight)
+    0.8 * weight      
+  end
+
+  #% PTN (Percent of the Protein)
+  def self.percent_ptn(kcal, tee)
+    (kcal * 100 / tee).round(2)
+  end
+
+  #RES. PTN (Residual of the Protein)
+  def self.res_ptn(tee)
+    (tee * 0.15).round(2)
+  end
+
+
 end
