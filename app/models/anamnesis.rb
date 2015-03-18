dea8ab294d8f460583370cfd308384593132c5ab
Yomestre <Yomestre@hotmail.com>
2015-03-18 13:21:17 -0300

    Calculos totais

--- app/models/anamnesis.rb
+++ app/models/anamnesis.rb
@@ -2,4 +2,16 @@ class Anamnesis < ActiveRecord::Base
   validates :medical_register_id, :morning_meal_time, :noon_meal_time, :evening_meal_time, :patient_id, presence:true
   belongs_to :patient, dependent: :destroy
   has_many :anamnesis_food_items
+  def total_carbohydrates(meal_time_field)
+    anamnesis_food_items.includes(:equivalent_food).where("meal_time == ?",meal_time_field).reduce(0){ |memo, obj| memo + (obj.equivalent_food.food_group.carbohydrate * obj.quantity)}
+  end
+  def total_proteins(meal_time_field)
+    anamnesis_food_items.includes(:equivalent_food).where("meal_time == ?",meal_time_field).reduce(0){ |memo, obj| memo + (obj.equivalent_food.food_group.proteins * obj.quantity)}
+  end
+  def total_lipids(meal_time_field)
+    anamnesis_food_items.includes(:equivalent_food).where("meal_time == ?",meal_time_field).reduce(0){ |memo, obj| memo + (obj.equivalent_food.food_group.lipids * obj.quantity)}
+  end
+  def total_kilocalories(meal_time_field)
+    anamnesis_food_items.includes(:equivalent_food).where("meal_time == ?",meal_time_field).reduce(0){ |memo, obj| memo + (obj.equivalent_food.food_group.kilocalories * obj.quantity)}
+  end
 end
