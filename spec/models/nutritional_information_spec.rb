7619af9fee73d4dae4e5417610ecb686c49adbbd
Alexandre <tecnoalpha@ig.com.br>
2015-03-17 21:16:41 -0300

    Criaçaõ do spec 'validate the TEE (Total Energy Expenditure)'

--- spec/models/nutritional_information_spec.rb
+++ spec/models/nutritional_information_spec.rb
@@ -8,44 +8,14 @@ RSpec.describe NutritionalInformation, :type => :model do
                           height: 1.8, 
                           weight: 80}}
 
-    before do
-      p = Patient.new(valid_patient)
-      @kcal = 1026
-      @af = 1.12
-      @tee = (1086 - (10.1 * p.age) + @af * (13.7 * p.weight) + (416 * p.height) - @kcal).round(2)
-    end
+    describe NutritionalInformation do
+      subject{ Patient.new(valid_patient) }
 
-    describe "validate the nutricional information" do
       it "validate the TEE (Total Energy Expenditure)" do
-        p = Patient.new(valid_patient)
-        tee = (1086 - (10.1 * p.age) + @af * (13.7 * p.weight) + (416 * p.height) - @kcal).round(2)
-        expect(tee).to eq (1733.32)
+        phisical_activity = 1.12
+        kcal = 1026
+        expect(NutritionalInformation.tee(subject.age, phisical_activity, subject.weight, subject.height, kcal)).to eq (1733.32)
       end
 
-        it "validate the PTN (Protein)" do
-          p = Patient.new(valid_patient)
-          ptn = 0.8 * p.weight
-          expect(ptn).to eq (64.0)
-        end
-
-        it "validate % PTN (Percent of the Protein)" do
-          pptn = (@kcal * 100 / @tee).round(2)
-          expect(pptn).to eq (59.19)
-        end
-
-        it "validate RES. PTN (Residual of the Protein)" do
-          rptn = (@tee * 0.15).round(2)
-          expect(rptn).to eq (260.0)
-        end
-
-        it "validate CHO (carbohydrate)" do
-          cho = (@tee * 0.60).round(2)
-          expect(cho).to eq (1039.99)
-        end
-
-        it "validate LIP (lipids)" do
-          lip = (@tee * 0.25).round(2)
-          expect(lip).to eq (433.33)
-        end
     end
 end
