c66252cfac031c617ceef2f73f5b7c4c7a40cbc9
Alexandre <tecnoalpha@ig.com.br>
2015-03-17 21:15:40 -0300

    Criação do model 'TEE (Total Energy Expenditure)'

--- app/models/nutritional_information.rb
+++ app/models/nutritional_information.rb
@@ -1,2 +1,7 @@
-class NutritionalInformation < ActiveRecord::Base
+class NutritionalInformation
+
+  #Total Energy Expenditure
+  def self.tee(age, phisical_activity, weight, height, kcal)
+    tee = (1086 - (10.1 * age) + phisical_activity * (13.7 * weight) + (416 * height) - kcal).round(2)
+  end
 end
