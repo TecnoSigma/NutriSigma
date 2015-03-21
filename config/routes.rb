0f8ac596457eab61d76f054bcf0d72e94dfa5e1f
Alexandre <tecnoalpha@ig.com.br>
2015-03-21 01:38:13 -0300

    Implementação de css no Menu Principal

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,14 @@ Rails.application.routes.draw do
   resources :nutritionist_profiles
   resources :nutritional_informations, only: [:show]
 
+
+  root 'home#index'
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  # root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
