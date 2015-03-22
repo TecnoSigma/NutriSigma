2b8cfa102ac84d3b39bb7ca5d11fc4dd250aee4b
Joao Almeida <joaorsalmeida@gmail.com>
2015-03-21 22:23:44 -0300

    using pysicial activity from patients anamnesis

--- app/controllers/nutritional_informations_controller.rb
+++ app/controllers/nutritional_informations_controller.rb
@@ -3,36 +3,37 @@ class NutritionalInformationsController < ApplicationController
     d = Date.today
     @data = d.strftime("%d/%m/%Y")
 
-    #Dados do Paciente
     @patient = Patient.find(params[:id])
+    nutri_info = NutritionalInformation.new(@patient)
 
     #Dados do Nutricionista
-    
 
     #Cálculos da Tabela de Reeducação Alimentar
-    @tee = NutritionalInformation.tee(@patient.age, @patient.anamnesis.physical_activity, @patient.weight, @patient.height, 1026)
-    @ptn = NutritionalInformation.ptn(@patient.weight)
-    @cho = NutritionalInformation.cho(@tee)
-    @lip = NutritionalInformation.lip(@tee)
+    kcal = 1026 # ISSO NAO DEVERIA TER SIDO INFORMADO PELO CLIENTE????
+    @tee = nutri_info.tee(kcal)
+    @ptn = nutri_info.ptn
+    @cho = nutri_info.cho(kcal)
+    @lip = nutri_info.lip(kcal)
 
     #Percentual Ideal de Consumo
-    demand_all_portions
+    @portions = nutri_info.demand_portions(kcal)
   end
 
   private
+  # esse metodo nao serva mais pra nada
   def demand_all_portions
-    @vegetais_a = NutritionalInformation.demand_portion("Vegetais A", 1026)
-    @vegetais_b = NutritionalInformation.demand_portion("Vegetais B", 1025)
-    @frutas = NutritionalInformation.demand_portion("Frutas", 1025)
-    @arroz = NutritionalInformation.demand_portion("Arroz", 1025)
-    @carnes = NutritionalInformation.demand_portion("Carnes", 1025)
-    @leites_e_derivados = NutritionalInformation.demand_portion("Leites e Derivados", 1025)
-    @desnatado_e_soja = NutritionalInformation.demand_portion("Desnatado e soja", 1025)
-    @gorduras = NutritionalInformation.demand_portion("Gorduras", 1025)
-    @embutidos = NutritionalInformation.demand_portion("Embutidos", 1025)
-    @feijoes = NutritionalInformation.demand_portion("Feijões", 1025)
-    @paes = NutritionalInformation.demand_portion("Pães", 1025)
-    @fibras = NutritionalInformation.demand_portion("Fibras", 1025)
-    @acucar = NutritionalInformation.demand_portion("Açúcar", 1025)
+    @vegetais_a = nutri_info.demand_portion("Vegetais A", 1026)
+    @vegetais_b = nutri_info.demand_portion("Vegetais B", 1025)
+    @frutas = nutri_info.demand_portion("Frutas", 1025)
+    @arroz = nutri_info.demand_portion("Arroz", 1025)
+    @carnes = nutri_info.demand_portion("Carnes", 1025)
+    @leites_e_derivados = nutri_info.demand_portion("Leites e Derivados", 1025)
+    @desnatado_e_soja = nutri_info.demand_portion("Desnatado e soja", 1025)
+    @gorduras = nutri_info.demand_portion("Gorduras", 1025)
+    @embutidos = nutri_info.demand_portion("Embutidos", 1025)
+    @feijoes = nutri_info.demand_portion("Feijões", 1025)
+    @paes = nutri_info.demand_portion("Pães", 1025)
+    @fibras = nutri_info.demand_portion("Fibras", 1025)
+    @acucar = nutri_info.demand_portion("Açúcar", 1025)
   end
 end
